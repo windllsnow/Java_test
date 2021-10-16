198972e3157a85aafe56c8e9d29481e6d1b31021
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-10-16 18:43:44 +0800

    Add files via upload

---
+++ java_first/ch2_03.java
@@ -0,0 +1,13 @@
+/*
+	程式實例ch2_3.java
+	作者:洪錦魁
+*/
+public class ch2_3 {
+	// main是程式的起點
+	public static void main(String[] args) {
+		// 字串輸出
+		System.out.println("Hello! Java");
+		System.out.println("I love Java");	// 第2筆字串輸出
+	}
+}
+
