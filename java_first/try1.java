c1ecf03614bbbccbc20ce5625c1b1d50af642d91
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-11-06 21:32:35 +0800

    Add files via upload

---
+++ java_first/try1.java
@@ -0,0 +1,22 @@
+/**
+ * exercise:ch2_4.java
+ * author:jk HUng
+ * 
+ */
+
+public class try1{
+    public static void main(String[] args){
+        System.out.println("hello! java");
+        System.out.println("I love java");
+    }
+
+
+
+
+}
+
+
+
+
+    
+
