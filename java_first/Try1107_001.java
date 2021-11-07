d89d4ca63cc31a67bd4a9ef61d39c6b952f1bb60
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-11-07 22:26:19 +0800

    Add files via upload

---
+++ java_first/Try1107_001.java
@@ -0,0 +1,22 @@
+//檔名  習慣命名
+public class Try1107_001 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        //變數  習慣命名
+         int appleBananaCup=1;
+        System.out.println("x="+appleBananaCup);
+        //常數  習慣命名
+        final int Y=55;
+        System.out.println("Y="+Y);
+        String aa="123_123";
+        String bb="234_234";
+        String cc="345_345";
+        //比較 一下
+        System.out.printf("hihi %s %s %s",aa,bb,cc);
+        System.out.println("\n");
+        System.out.println("hihi"+" "+aa+" "+bb+" "+cc);
+
+
+    }
+    
+}
