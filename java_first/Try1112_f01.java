4091ccbce4ba391e689aa6d82d4df96f97393717
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-11-12 00:14:59 +0800

    Add files via upload

---
+++ java_first/Try1112_f01.java
@@ -0,0 +1,18 @@
+/*
+*package....
+*import...
+*class ___ {
+    
+}
+*/
+
+
+
+import kk_tt.Begining;
+public class Try1112_f01 {
+    public static void main(String[] args){
+        Begining beginning= new Begining();
+        beginning.getDate();
+    }
+    
+}
