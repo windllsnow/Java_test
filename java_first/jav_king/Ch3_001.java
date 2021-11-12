6aa623f7fd60df603bdec8afc3b9d82f73a67672
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-11-12 20:14:34 +0800

    Add files via upload

---
+++ java_first/jav_king/Ch3_001.java
@@ -0,0 +1,15 @@
+package jav_king;
+    public class  Ch3_001{
+        public static void main(String[] args){
+            char ch;
+            ch='a';
+            System.out.println(ch);// 輸出 a
+            ch='洪';
+            System.out.println(ch); //輸 出 洪
+            ch=65;
+            System.out.println(ch);//but  打 數字 視為Unicode 碼 --> 對應的字元
+        }
+
+
+    }
+
---
+++ java_first/jav_king/Ch3_002.java
@@ -0,0 +1,17 @@
+package jav_king;
+
+public class Ch3_002 {
+    
+    /** 
+    *   '\ u XXXX'  用逸出字元序列 方式處理 Unicode碼  X是16進位
+    */
+    public static void main(String[] args) {
+        char ch;
+        ch= '\u0041';
+        System.out.println(ch);
+        ch='\u9B41';
+        System.out.println(ch);
+    }
+
+    
+}
